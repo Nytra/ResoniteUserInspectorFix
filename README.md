cb95887ce58ba2ec6026b7cee0557de624e90aa2
Nytra <14206961+Nytra@users.noreply.github.com>
2025-01-20 14:32:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Distant Component Optimizer
+=============================
+
+Limits the update frequency of components that are too far away from you. Configurable.
+
+This has been tested by a couple of people and the performance gains are pretty decent. I saw around +10 FPS in multiple worlds. You may get more or less depending on the world.
+
+The main side effect of the mod is that animated things in the distance will stutter. This is normal.
+
+As far as I know this doesn't break anything but there could still be unknown issues at this time.
+
+## Installation
+
+1. Install [MonkeyLoader Resonite GamePack](https://github.com/ResoniteModdingGroup/MonkeyLoader.GamePacks.Resonite)
+2. [Download the mod](https://github.com/Nytra/ResoniteDistantComponentOptimizer/releases/latest)
+3. Place it in your `MonkeyLoader/Mods` folder inside your Resonite installation
---
+++ UserInspectorFix/BasicPatcher.cs
@@ -0,0 +1,30 @@
+﻿using FrooxEngine;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using MonkeyLoader.Resonite;
+
+namespace UserInspectorFix
+{
+    [HarmonyPatchCategory(nameof(UserInspectorFix))]
+    [HarmonyPatch(typeof(UserInspector), "OnAttach")]
+    internal class UserInspectorFix : ResoniteMonkey<UserInspectorFix>
+    {
+        public override bool CanBeDisabled => true;
+        private static void Postfix(UserInspector __instance, SyncRef<Slot> ____userListContentRoot)
+        {
+            if (!Enabled) return;
+            if (__instance.World.IsAuthority) return;
+            if (____userListContentRoot.Target.ChildrenCount != 0) return; // just in case this gets fixed in the future
+            foreach (var user in __instance.World.AllUsers)
+            {
+                __instance.RunSynchronously(delegate
+                {
+                    Slot slot = ____userListContentRoot.Target.AddSlot("User");
+                    slot.PersistentSelf = false;
+                    slot.AttachComponent<VerticalLayout>().PaddingTop.Value = 4f;
+                    slot.AttachComponent<UserInspectorItem>().Setup(user);
+                });
+            }
+        }
+    }
+}
